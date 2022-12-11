8e410b0c3ffd982134a445b3b0dfc01b1a46b60a
Amyliciouz <amakapromise15@gmail.com>
2022-12-11 12:49:12 -0800

    commit 0x0F-python-object_relational_mapping files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# script that lists all states from the database hbtn_0e_0_usa
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+        """
+            lists all states from the database hbtn_0e_0_usa
+                in ascending order by states.id
+                    """
+                        db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+                            cursor = db.cursor()
+                                cursor.execute("SELECT * FROM `states`")
+                                    [print(state) for state in cursor.fetchall()]]
