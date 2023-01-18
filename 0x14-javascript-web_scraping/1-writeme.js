4dca58230fcabb340587dc1599791fb155edbfe3
Amyliciouz <amakapromise15@gmail.com>
2023-01-17 20:57:46 -0800

    Commit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,5 +5,5 @@ const file = process.argv[2];
 const fInput = process.argv[3];
 
 fs.writeFile(file, fInput, 'utf-8', (err) => {
-	  if (err) console.log(err);
+  if (err) console.log(err);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,8 +5,8 @@ const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 req.get(url, function (err, response, body) {
-	  if (err) console.error(err); else {
-	const jsonbody = JSON.parse(body);
-	console.log(jsonbody.title);
-	}
+  if (err) console.error(err); else {
+    const jsonbody = JSON.parse(body);
+    console.log(jsonbody.title);
+  }
 });
